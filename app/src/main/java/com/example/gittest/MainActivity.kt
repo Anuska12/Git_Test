13f4cd491ba524780582066ab987aecd01054363
DESKTOP-6RQ1N96\Anushka <anushka.zaman@trustaxiatapay.com>
2022-06-23 15:12:00 +0600

    Git test Application

---
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.gittest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val v = findViewById<TextView>(R.id.tv1)
+    }
+}
