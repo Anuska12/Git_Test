9a856f4bfc4fa5b53c3af9863c418caa1dee6ac4
DESKTOP-6RQ1N96\Anushka <anushka.zaman@trustaxiatapay.com>
2022-06-23 15:51:52 +0600

    updated button

--- app/src/main/java/com/example/gittest/MainActivity.kt
+++ app/src/main/java/com/example/gittest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.gittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val v = findViewById<TextView>(R.id.tv1)
+        val b = findViewById<Button>(R.id.button1)
     }
 }
